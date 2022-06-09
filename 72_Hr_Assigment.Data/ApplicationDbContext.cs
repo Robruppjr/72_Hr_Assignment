6150a6e6767285e16dfea67c8e0e14eed105786b
Rob Rupp <robruppjr@gmail.com>
2022-06-09 19:38:40 -0400

    Reply Entity

--- 72_Hr_Assigment.Data/ApplicationDbContext.cs
+++ 72_Hr_Assigment.Data/ApplicationDbContext.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace _72_Hr_Assigment.Data
-{
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
     }
-}
--- 72_Hr_Assigment.Data/Entities/ReplyEntity.cs
+++ 72_Hr_Assigment.Data/Entities/ReplyEntity.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,11 @@ namespace _72_Hr_Assigment.Data.Entities
 {
     public class ReplyEntity
     {
-        
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(CommentId))]
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        // public Guid AuthorId { get; set; }
     }
 }
