eccb386081ed2438f587bbd637f40c3566add980
Rob Rupp <robruppjr@gmail.com>
2022-06-09 20:55:45 -0400

    Savaing point

--- 72_Hr_Assigment.Data/Entities/ReplyEntity.cs
+++ 72_Hr_Assigment.Data/Entities/ReplyEntity.cs
@@ -11,9 +11,11 @@ namespace _72_Hr_Assigment.Data.Entities
     {
         [Key]
         public int Id { get; set; }
-        [ForeignKey(nameof(CommentId))]
+        [ForeignKey(nameof(Comment))]
         public int CommentId { get; set; }
+        [Required]
         public string Text { get; set; }
+        public List<ReplyEntity> Replies {get; set;}
         // public Guid AuthorId { get; set; }
     }
 }
